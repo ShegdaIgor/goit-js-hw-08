224ce9fa4ad7e42b4ff42d8b1d8bf430b542f43e
Shegda Igor <igorbeef@gmail.com>
2022-11-17 16:19:23 +0200

    end hw-8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
+
+const slideGallery = document.querySelector('.gallery');
+
+function createGallaryMarkup(items) {
+  return items
+    .map(
+      item => `<li>
+                  <a class="gallery__item" href="${item.original}">
+                    <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+                </a>
+              </li>`
+    )
+    .join('');
+}
+
+slideGallery.innerHTML = createGallaryMarkup(galleryItems);
+new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
